73a81ecd6c2c45d01f2ae7e4d2f121a0bc00f212
Andrea Musmarra <amusmarra@ymail.com>
2024-03-05 10:21:49 +0100

    Added config file

---
+++ config/red-thread.php
@@ -0,0 +1,40 @@
+<?php
+
+	return [
+
+		/*
+		|--------------------------------------------------------------------------
+		| Check for attribute
+		|--------------------------------------------------------------------------
+		|
+		| This value indicates whether to check the attribute "#[RedThread]"
+		| used to mark a method as relationship or not. If set to "false"
+		| the package will check the return type of the method.
+		|
+		*/
+
+		'check_for_attribute' => true,
+
+		/*
+		|--------------------------------------------------------------------------
+		| Relationships types
+		|--------------------------------------------------------------------------
+		|
+		| This array contains all relationship types.
+		|
+		*/
+
+		'relationships' => [
+			Illuminate\Database\Eloquent\Relations\HasOne::class,
+			Illuminate\Database\Eloquent\Relations\HasMany::class,
+			Illuminate\Database\Eloquent\Relations\MorphTo::class,
+			Illuminate\Database\Eloquent\Relations\MorphOne::class,
+			Illuminate\Database\Eloquent\Relations\MorphMany::class,
+			Illuminate\Database\Eloquent\Relations\BelongsTo::class,
+			Illuminate\Database\Eloquent\Relations\MorphToMany::class,
+			Illuminate\Database\Eloquent\Relations\HasOneThrough::class,
+			Illuminate\Database\Eloquent\Relations\BelongsToMany::class,
+			Illuminate\Database\Eloquent\Relations\HasManyThrough::class,
+		],
+
+	];
